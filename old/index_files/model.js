2d3e0691e353f7ec20483ebcdddf94ae4ef28825
weikaichenusc <weikaich@usc.edu>
2018-08-04 19:26:20 -0700

    new webpage

--- old/index_files/model.js
+++
@@ -1,7 +0,0 @@
-function publishSettings() {
-    var widgetId = Wix.Utils.getInstanceId() + '--' + Wix.Utils.getCompId();
-    $.get($('base').attr('href') + 'en/settings/publish/?id=' + widgetId + "&compid=" + Wix.Utils.getCompId() + '&instance='+instance, function(data) {
-        // callback
-    }).fail(function(jqXHR, textStatus) {
-    });
-}
